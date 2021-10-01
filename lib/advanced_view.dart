8839a703adb9abefff401ff9b7de54416f140e1e
2018331113 <muhitmahmud404@gmail.com>
2021-10-02 02:56:56 +0600

    5th update

---
+++ lib/advanced_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdvancedView extends StatefulWidget {
+  const AdvancedView({Key? key}) : super(key: key);
+
+  @override
+  _AdvancedViewState createState() => _AdvancedViewState();
+}
+
+class _AdvancedViewState extends State<AdvancedView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
